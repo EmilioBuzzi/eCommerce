33d1159dcec424049ac8e235ae9abbdb20eb4f49
emiliobuzzi <enbuzzi@gmail.com>
2022-10-27 19:12:31 -0300

    3° Commit

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(3000, () => {
-console.log("Servidor funcionando");
-})
+// app.listen(3000, () => {
+// console.log("Servidor funcionando");
+// })
 
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => console.log("Servidor corriendo en el puerto ${port}"));
+    
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/views/home.html");
 })
